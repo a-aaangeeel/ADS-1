5704fe00a15c60bbb24d9746f95b1b23cd8a18fd
Nana <160034837+a-aaangeeel@users.noreply.github.com>
2024-03-01 21:31:07 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,56 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+  	bool result = true;
+	if (value > 1) {
+		int j = 0;
+		for (j = 2; j * j <= value; j++) {
+			if (value % j == 0) {
+				result = false;
+				break;
+			}
+		}
+	}
+	else result = false;
+
+	return result;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+  	uint64_t result = 0; 
+	uint64_t temp_n = n;
+	while (1) {
+		if (checkPrime(temp_n)) {
+			result = temp_n;
+			break;
+		} 
+		temp_n++;
+	} 
+	return result;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+  	uint64_t result = 0;
+	uint64_t temp_n = value+1;
+	while (1) {
+		if (checkPrime(temp_n)) {
+			result = temp_n;
+			break;
+		}
+		temp_n++;
+	}
+	return result;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+  	uint64_t tempSum = 0;
+	for (uint64_t j = 2; j < hbound; j++) {
+		if (checkPrime(j)) {
+			tempSum = tempSum + j;
+		}
+	}
+	return tempSum;
 }
